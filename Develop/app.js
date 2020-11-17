95477cb7ff7a370fbad4c814c89a4e739e3fc315
Megan Carnaghi <carnagh5@gmail.com>
2020-11-17 13:54:43 -0500

    Updates to README file.

--- Develop/app.js
+++ Develop/app.js
@@ -10,53 +10,157 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Creating an empty array to push the new team members to
+const newTeam = [];
+
+// Invoking the promptToAddNewEmployee Function
+promptToAddNewEmployee();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptToAddNewEmployee(){
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "add",
+      message: "Do you want to add a new employee to your team?",
+      choices: [
+        "yes",
+        "no",
+      ]
+    }
+  ])
+  .then(function(data) {
+    if(data.add === "yes") {
+      addEmployeeToTeam();
+    } else {
+      writeFile();
+    }
+  })
+}
 
-// Prompting the user to get the employee's role
-function promptUser() {
-    return inquirer.prompt([
+// Function to add a new employee to the team
+function addEmployeeToTeam() {
+    inquirer.prompt([
         {
             type: "list",
             name: "role",
             message: "What role does the employee have?",
             choices: [
-                "Manager",
-                "Engineer",
-                "Intern"
+                "manager",
+                "engineer",
+                "intern"
             ]
         }
     ])
+    .then(function(data) {
+      if (data.role === "manager") {
+          addManager();
+      } else if (data.role === "engineer") {
+          addEngineer();
+      } else {
+          addIntern();
+      }
+  });
 }
 
-
-function promptUser() {
-    return inquirer.prompt([
+// Function to ask the appropriate questions and add a new manager to the array
+function addManager() {
+    inquirer.prompt([
       {
         type: "input",
         name: "name",
-        message: "What is the employee's name"
+        message: "What is the manager's name?"
       },
       {
         type: "input",
         name: "id",
-        message: "What is the employee's ID?"
+        message: "What is the manager's id?"
       },
       {
         type: "input",
         name: "email",
-        message: "What is the employee's email address?"
+        message: "What is the manager's email address?"
       },
-    ]);
+      {
+      type: "input",
+      name: "office",
+      message: "What is the manager's office number?"
+  }
+    ])
+    .then(function(data) {
+      const employee = new Manager(data.name, data.id, data.email, data.office);
+      newTeam.push(employee);
+      promptToAddNewEmployee()
+  });
 }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// Invoking the render function
-render()
+// Function to ask the appropriate questions and add a new engineer to the array
+function addEngineer() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's id?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email address?"
+    },
+    {
+    type: "input",
+    name: "github",
+    message: "What is the manager's GitHub username?"
+}
+  ])
+  .then(function(data) {
+    const employee = new Engineer(data.name, data.id, data.email, data.github);
+    newTeam.push(employee);
+    promptToAddNewEmployee()
+});
+}
 
+
+// Function to ask the appropriate questions and add a new intern to the array
+function addIntern() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's id?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email address?"
+    },
+    {
+    type: "input",
+    name: "github",
+    message: "What is the name of the intern's school?"
+}
+  ])
+  .then(function(data) {
+    const employee = new Intern (data.name, data.id, data.email, data.school);
+    newTeam.push(employee);
+    promptToAddNewEmployee()
+});
+}
+
+// a function to stop the prompts and generate the files
+function writeFile() {
+    console.log("Creating your file. Check output folder for result.");
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -64,6 +168,12 @@ render()
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+    fs.writeFileSync(outputPath, render(newTeam), "utf-8");
+}
+
